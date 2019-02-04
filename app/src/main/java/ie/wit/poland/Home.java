297b146257620394667917c704b7d34b29f78d5e
unknown <kacperwoloszyn4@gmail.com>
2019-02-04 17:57:21 +0000

    Bug fixes

--- app/src/main/java/ie/wit/poland/Home.java
+++ app/src/main/java/ie/wit/poland/Home.java
@@ -1,10 +1,11 @@
 package ie.wit.poland;
 
+
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -50,5 +51,4 @@ public class Home extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
