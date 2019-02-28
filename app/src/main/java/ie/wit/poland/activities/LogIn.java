35ce6fbb5f6c11e48b9c4252c6bbeda62b5ce7f6
unknown <kacperwoloszyn4@gmail.com>
2019-02-28 01:41:26 +0000

    Register working / prior layout changes

--- app/src/main/java/ie/wit/poland/activities/LogIn.java
+++ app/src/main/java/ie/wit/poland/activities/LogIn.java
@@ -1,15 +1,27 @@
 package ie.wit.poland.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import ie.wit.poland.R;
+import ie.wit.poland.main.LandmarkApp;
 
 public class LogIn extends AppCompatActivity {
+    public LandmarkApp app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in);
+        app = (LandmarkApp) getApplication();
     }
-}
+
+
+    public void signUp(View v)
+    {
+        startActivity(new Intent(this,Register.class));
+    }
+    }
+
--- app/src/main/java/ie/wit/poland/activities/Register.java
+++ app/src/main/java/ie/wit/poland/activities/Register.java
@@ -1,15 +1,86 @@
 package ie.wit.poland.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import ie.wit.poland.R;
 
 public class Register extends AppCompatActivity {
 
+    EditText registeremail, registerpassword;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+        registeremail = (EditText) findViewById(R.id.registeremail);
+        registerpassword = (EditText) findViewById(R.id.registerpassword);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void goToLogin(View v) {
+        startActivity(new Intent(this, LogIn.class));
+    }
+
+
+    public void register(View v) {
+        registerUser();
+    }
+
+    private void registerUser() {
+        String email = registeremail.getText().toString();
+        String password = registerpassword.getText().toString();
+
+        if (email.isEmpty()) {
+            registeremail.setError("Please put in email");
+            registeremail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            registeremail.setError("Please enter a valid email");
+            registeremail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            registerpassword.setError("Password is required");
+            registerpassword.requestFocus();
+            return;
+        }
+        if (password.length()<6){
+            registerpassword.setError("Password must contain more than 6 characters");
+            registerpassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            //Annonymous class to see if reg is completed successfully
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"UserRegisteredSuccessfully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
+
+
+
+
+
