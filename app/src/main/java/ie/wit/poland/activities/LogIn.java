c631940a067a5a34c21f49c7c2620271a9d96812
unknown <kacperwoloszyn4@gmail.com>
2019-02-28 00:15:54 +0000

    Adding logo image/ log in / reg

---
+++ app/src/main/java/ie/wit/poland/activities/LogIn.java
@@ -0,0 +1,15 @@
+package ie.wit.poland.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.wit.poland.R;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+    }
+}
