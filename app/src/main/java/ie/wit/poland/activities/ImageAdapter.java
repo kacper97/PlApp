c5922b3a039a79ee716a92d6051579d8c38dfda1
unknown <kacperwoloszyn4@gmail.com>
2019-04-29 18:51:03 +0100

    Recycler Adapter for images

---
+++ app/src/main/java/ie/wit/poland/activities/ImageAdapter.java
@@ -0,0 +1,61 @@
+package ie.wit.poland.activities;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import ie.wit.poland.R;
+import ie.wit.poland.models.Image;
+
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
+    private Context mContext;
+    private List<Image> mImages;
+
+    public ImageAdapter(Context context, List<Image> uploads) {
+        mContext = context;
+        mImages = uploads;
+    }
+
+    @Override
+    public ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.image_item, parent, false);
+        return new ImageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ImageViewHolder holder, int position) {
+        Image uploadCurrent = mImages.get(position);
+        holder.textViewName.setText(uploadCurrent.getName());
+        Picasso.get()
+                .load(uploadCurrent.getImageUrl())
+                .fit()
+                .centerCrop()
+                .into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImages.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+        public TextView textViewName;
+        public ImageView imageView;
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.text_view_name);
+            imageView = itemView.findViewById(R.id.image_view_upload);
+        }
+    }
+}
