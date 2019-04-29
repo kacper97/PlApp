9379e55b1d0253667f7258388ac1e55f48a3e988
unknown <kacperwoloszyn4@gmail.com>
2019-04-29 18:10:06 +0100

    image firebase connection

--- app/src/main/java/ie/wit/poland/activities/ImageActivity.java
+++ app/src/main/java/ie/wit/poland/activities/ImageActivity.java
@@ -1,20 +1,38 @@
 package ie.wit.poland.activities;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.shashank.sony.fancytoastlib.FancyToast;
 import com.squareup.picasso.Picasso;
 
 import ie.wit.poland.R;
+import ie.wit.poland.models.Image;
 
 public class ImageActivity extends AppCompatActivity {
 
@@ -27,6 +45,9 @@ public class ImageActivity extends AppCompatActivity {
     private ProgressBar mProgressBar;
     private Uri mImageUri;
 
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +59,8 @@ public class ImageActivity extends AppCompatActivity {
         mImageView = findViewById(R.id.image_view);
         mProgressBar = findViewById(R.id.progress_bar);
 
+        mStorageRef = FirebaseStorage.getInstance().getReference("images");
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("images");
 
         mButtonChooseImage.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -49,6 +72,7 @@ public class ImageActivity extends AppCompatActivity {
         mButtonUpload.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                uploadFile();
             }
         });
 
@@ -57,6 +81,71 @@ public class ImageActivity extends AppCompatActivity {
             public void onClick(View v){
             }
         });
+    }
+
+    //get the file extansion for the image
+    //MIME stands for "Multipurpose Internet Mail Extensions. It's a way of identifying files on the Internet according to their nature and format.
+    private String getFileExtension (Uri uri){
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void uploadFile() {
+        if(mImageUri != null){
+            //time + jpg/png
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+            + "." + getFileExtension(mImageUri));
+
+            fileReference.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //delays the reset of progress bar
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mProgressBar.setProgress(0);
+                        }
+                    }, 5000);
+
+                    if (mImageUri != null) {
+                        mStorageRef.putFile(mImageUri).continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                            @Override
+                            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                                if (!task.isSuccessful()) {
+                                    throw task.getException();
+                                }
+                                return mStorageRef.getDownloadUrl();
+                            }
+                        })
+
+                                .addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Uri> task) {
+                                        if (task.isSuccessful()) {
+                                            Uri downloadUri = task.getResult();
+                                            Image image = new Image(mEditTextFileName.getText().toString().trim(),
+                                                    downloadUri.toString());
+                                            mDatabaseRef.push().setValue(image);
+                                            FancyToast.makeText(ImageActivity.this, "Upload complete", Toast.LENGTH_LONG, FancyToast.SUCCESS, false).show();
+                                        } else {
+                                            FancyToast.makeText(ImageActivity.this, "upload failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+                                        }
+                                    }
+                                })
+
+
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        FancyToast.makeText(ImageActivity.this, e.getMessage(), Toast.LENGTH_LONG, FancyToast.ERROR, true).show();
+                                    }
+                                });
+                    }
+                }
+        });
+        }
     }
 
     private void openFileChooser() {
@@ -72,7 +161,7 @@ public class ImageActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode== PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() !=null) {
             mImageUri = data.getData();
-            Picasso.with(this).load(mImageUri).into(mImageView);
+            Picasso.get().load(mImageUri).fit().into(mImageView);
             //mImageView.setImageURI(mImageUri);
             FancyToast.makeText(getApplicationContext(),"Image Added to View",FancyToast.LENGTH_LONG,FancyToast.INFO,true).show();
         }
